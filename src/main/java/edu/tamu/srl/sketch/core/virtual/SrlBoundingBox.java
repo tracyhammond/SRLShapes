5073f88b41ad3b5a459fed01537c0d2a47c3864d
dtracers <dtracers@gmail.com>
2014-11-04 18:40:41 -0600

    Added constructors for bounding box

--- src/main/java/edu/tamu/srl/sketch/core/virtual/SrlBoundingBox.java
+++ src/main/java/edu/tamu/srl/sketch/core/virtual/SrlBoundingBox.java
@@ -25,4 +25,71 @@ public class SrlBoundingBox extends SrlVirtualObject {
      * Top Left should be less than the Bottom Right.
      */
     private SrlPoint mBottomRightCorner;
+
+    /**
+     * Constructor takes two points, and constructs a horizontal rectangle from this.
+     *
+     * @param topLeftCorner The point that is assigned as the top left corner. Should be lower in value.
+     * @param bottomRightCorner The point that is assigned as the bottom right corner.  Should be larger in value.
+     */
+    public SrlBoundingBox(final SrlPoint topLeftCorner, final SrlPoint bottomRightCorner) {
+        setTopLeftCorner(topLeftCorner);
+        setBottomRightCorner(bottomRightCorner);
+    }
+
+    /**
+     * Constructor.
+     *
+     * @param minX the smallest x value.
+     * @param minY the smallest y value.
+     * @param maxX the largest x value.
+     * @param maxY the largest y value.
+     */
+    public SrlBoundingBox(final double minX, final double minY, final double maxX, final double maxY) {
+        this(new SrlPoint(minX, minY), new SrlPoint(maxX, maxY));
+    }
+
+    /**
+     * Copy constructor.
+     * @param srlBoundingBox object that is being copied.
+     */
+    public SrlBoundingBox(final SrlBoundingBox srlBoundingBox) {
+        super(srlBoundingBox);
+    }
+
+    /**
+     * Gets the bottom right corner.
+     *
+     * @return The bottom right corner.
+     */
+    public final SrlPoint getBottomRightCorner() {
+        return mBottomRightCorner;
+    }
+
+    /**
+     * Sets the bottom right corner.
+     *
+     * @param bottomRightCorner The {@link edu.tamu.srl.sketch.core.virtual.SrlPoint} that represents the bottom right corner.
+     */
+    public final void setBottomRightCorner(final SrlPoint bottomRightCorner) {
+        mBottomRightCorner = bottomRightCorner;
+    }
+
+    /**
+     * Gets the top left corner point.
+     *
+     * @return The top left corner.
+     */
+    public final SrlPoint getTopLeftCorner() {
+        return mTopLeftCorner;
+    }
+
+    /**
+     * Sets the top left corner.
+     *
+     * @param topLeftCorner The {@link edu.tamu.srl.sketch.core.virtual.SrlPoint} that represents the top left corner.
+     */
+    public final void setTopLeftCorner(final SrlPoint topLeftCorner) {
+        mTopLeftCorner = topLeftCorner;
+    }
 }
