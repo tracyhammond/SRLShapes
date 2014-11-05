9cb6bf60ec5880095368f98a1192ab3c1dac33f8
dtracers <dtracers@gmail.com>
2014-11-04 21:37:20 -0600

    Added constructors for stroke

--- src/main/java/edu/tamu/srl/sketch/core/object/SrlStroke.java
+++ src/main/java/edu/tamu/srl/sketch/core/object/SrlStroke.java
@@ -7,6 +7,7 @@ import edu.tamu.srl.sketch.core.tobenamedlater.SrlPen;
 import edu.tamu.srl.sketch.core.virtual.SrlPoint;
 
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by gigemjt on 11/3/14.
@@ -44,4 +45,111 @@ public class SrlStroke extends SrlObject {
      * Device the stroke was drawn on.
      */
     private final SrlDevice mDevice;
+
+    /**
+     * Default constructor.
+     */
+    public SrlStroke() {
+        super();
+        this.mAuthor = null;
+        this.mPen = null;
+        this.mDevice = null;
+    }
+
+    /**
+     * Accepts values that can only be set during construction.
+     *
+     * @param time          The time the shape was originally created.
+     * @param id            The unique identifier of the shape.
+     * @param isUserCreated True if the user created the stroke instead of the computer.
+     */
+    public SrlStroke(final long time, final UUID id, final boolean isUserCreated) {
+        super(time, id, isUserCreated);
+        this.mAuthor = null;
+        this.mPen = null;
+        this.mDevice = null;
+    }
+
+    /**
+     * A copy constructor.
+     *
+     * Copies all values from the given object.
+     *
+     * @param o the object that is being copied.
+     */
+    public SrlStroke(final SrlStroke o) {
+        super(o);
+        this.mAuthor = o.getAuthor();
+        this.mPen = o.getPen();
+        this.mDevice = o.getDevice();
+    }
+
+    /**
+     * Default constructor.
+     * @param author Who made the stroke.
+     * @param pen What made the stroke.
+     * @param device where was the stroke made.
+     */
+    public SrlStroke(final SrlAuthor author, final SrlPen pen, final SrlDevice device) {
+        super();
+        this.mAuthor = author;
+        this.mPen = pen;
+        this.mDevice = device;
+    }
+
+    /**
+     * Accepts values that can only be set during construction.
+     *
+     * @param time The time the shape was originally created.
+     * @param id   The unique identifier of the shape.
+     * @param author Who made the stroke.
+     * @param pen What made the stroke.
+     * @param device where was the stroke made.
+     */
+    public SrlStroke(final long time, final UUID id, final SrlAuthor author, final SrlPen pen,
+            final SrlDevice device) {
+        super(time, id);
+        this.mAuthor = author;
+        this.mPen = pen;
+        this.mDevice = device;
+    }
+
+    /**
+     * Accepts values that can only be set during construction.
+     *
+     * @param time          The time the shape was originally created.
+     * @param id            The unique identifier of the shape.
+     * @param isUserCreated True if the user created the stroke instead of the computer.
+     * @param author Who made the stroke.
+     * @param pen What made the stroke.
+     * @param device where was the stroke made.
+     */
+    public SrlStroke(final long time, final UUID id, final boolean isUserCreated,
+            final SrlAuthor author, final SrlPen pen, final SrlDevice device) {
+        super(time, id, isUserCreated);
+        this.mAuthor = author;
+        this.mPen = pen;
+        this.mDevice = device;
+    }
+
+    /**
+     * @return Who made the stroke.
+     */
+    public final SrlAuthor getAuthor() {
+        return mAuthor;
+    }
+
+    /**
+     * @return What made the stroke.
+     */
+    public final SrlPen getPen() {
+        return mPen;
+    }
+
+    /**
+     * @return Where was the stroke made.
+     */
+    public final SrlDevice getDevice() {
+        return mDevice;
+    }
 }
