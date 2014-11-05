2861b25b47aad216713a8d163bab1f7c814c7ee2
dtracers <dtracers@gmail.com>
2014-11-04 18:40:05 -0600

    Added constructors to virtual objects

--- src/main/java/edu/tamu/srl/sketch/core/abstracted/SrlVirtualObject.java
+++ src/main/java/edu/tamu/srl/sketch/core/abstracted/SrlVirtualObject.java
@@ -1,5 +1,7 @@
 package edu.tamu.srl.sketch.core.abstracted;
 
+import java.util.UUID;
+
 /**
  * Created by gigemjt on 11/3/14.
  * @author gigemjt
@@ -9,5 +11,35 @@ package edu.tamu.srl.sketch.core.abstracted;
  * Virtual objects are useful but should not appear naturally on a sketch surface.
  * Virtual objects are not able to be recognized and should not be sent to a recognition system.
  */
-public class SrlVirtualObject extends SrlComponent {
+public abstract class SrlVirtualObject extends SrlComponent {
+
+    /**
+     * Default constructor.
+     * <p/>
+     * Creates an object with an id and a time.
+     */
+    public SrlVirtualObject() {
+        super();
+    }
+
+    /**
+     * A copy constructor.
+     * <p/>
+     * Copies all values from the given object.
+     *
+     * @param o the object that is being copied.
+     */
+    public SrlVirtualObject(final SrlVirtualObject o) {
+        super(o);
+    }
+
+    /**
+     * Accepts values that can only be set during construction.
+     *
+     * @param time The time the shape was originally created.
+     * @param id   The unique identifier of the shape.
+     */
+    public SrlVirtualObject(final long time, final UUID id) {
+        super(time, id);
+    }
 }
