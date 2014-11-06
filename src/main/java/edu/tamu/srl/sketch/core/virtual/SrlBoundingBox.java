a0dafc7689b6fe99da57cdb82897004ba9f9577a
dtracers <dtracers@gmail.com>
2014-11-06 01:31:52 -0600

    made the bounding box compile

--- src/main/java/edu/tamu/srl/sketch/core/virtual/SrlBoundingBox.java
+++ src/main/java/edu/tamu/srl/sketch/core/virtual/SrlBoundingBox.java
@@ -1,5 +1,6 @@
 package edu.tamu.srl.sketch.core.virtual;
 
+import edu.tamu.srl.sketch.core.abstracted.SrlComponent;
 import edu.tamu.srl.sketch.core.abstracted.SrlVirtualObject;
 
 /**
@@ -91,5 +92,96 @@ public class SrlBoundingBox extends SrlVirtualObject {
      */
     public final void setTopLeftCorner(final SrlPoint topLeftCorner) {
         mTopLeftCorner = topLeftCorner;
+    }
+
+    /**
+     * Translate the object by the amount x,y.
+     *
+     * @param x the amount in the x direction to move the object by.
+     * @param y the amount in the y direction to move the object by.
+     */
+    @SuppressWarnings("checkstyle:designforextension")
+    @Override public void translate(final double x, final double y) {
+        getTopLeftCorner().translate(x, y);
+        getBottomRightCorner().translate(x, y);
+    }
+
+    /**
+     * Scales the SComponent by the given x- and y-factor.
+     *
+     * @param xFactor the x-factor
+     * @param yFactor the y-factor
+     */
+    @SuppressWarnings("checkstyle:designforextension")
+    @Override public void scale(final double xFactor, final double yFactor) {
+        getBottomRightCorner().scale(xFactor, yFactor);
+        getTopLeftCorner().scale(xFactor, yFactor);
+    }
+
+    /**
+     * Rotates the SComponent from the given x- and y-coordinate.
+     *
+     * @param radians the number of radians to rotate
+     * @param xCenter the x-coordinate to rotate from
+     * @param yCenter the y-coordinate to rotate from
+     */
+    @SuppressWarnings("checkstyle:designforextension")
+    @Override public void rotate(final double radians, final double xCenter, final double yCenter) {
+        getTopLeftCorner().rotate(radians, xCenter, yCenter);
+        getBottomRightCorner().rotate(radians, xCenter, yCenter);
+    }
+
+    /**
+     * @return A cloned object that is an instance of {@link SrlComponent}.  This cloned object is only a shallow copy.
+     */
+    @SuppressWarnings("checkstyle:designforextension")
+    @Override public Object clone() {
+        return new SrlBoundingBox(this);
+    }
+
+    /**
+     * @return performs a deep clone of the object cloning all objects contained as well.
+     */
+    @SuppressWarnings("checkstyle:designforextension")
+    @Override public SrlComponent deepClone() {
+        return new SrlBoundingBox(this);
+    }
+
+    /**
+     * Performs a shallow equals.
+     * By default this is called by the {@link #equals(Object)} method.
+     *
+     * @param other the other SrlObject.
+     * @return true if content is equal, false otherwise
+     */
+    @SuppressWarnings("checkstyle:designforextension")
+    @Override public boolean shallowEquals(final SrlComponent other) {
+        throw new UnsupportedOperationException("need to implement this");
+    }
+
+    /**
+     * Looks deep into two components to check equality.
+     * It would probably be smart to also have this call shallowEquals.
+     *
+     * @param other the other SrlObject.
+     * @return true if content is equal, false otherwise
+     */
+    @SuppressWarnings("checkstyle:designforextension")
+    @Override public boolean deepEquals(final SrlComponent other) {
+        throw new UnsupportedOperationException("need to implement this");
+    }
+
+    /**
+     * Get a Point that corresponds to the center of this component. The
+     * only thing that will be stored in this Point are X and Y values. Thus,
+     * you should /only/ use the interface's getX() and getY() methods, and
+     * should not cast the point to a concrete implementation. Time of the point
+     * is set to 0.
+     *
+     * @return the Point with X and Y coordinates set to the center of this
+     * bounding box.
+     */
+    @Override public final SrlPoint getCenterPoint() {
+        throw new UnsupportedOperationException("need to implement this");
     }
 }
