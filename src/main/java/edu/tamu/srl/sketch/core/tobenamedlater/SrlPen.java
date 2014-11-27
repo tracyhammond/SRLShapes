e666013c6728dd448cd6944b331892683159b651
dtracers <dtracers@gmail.com>
2014-11-27 13:48:15 -0600

    Added a hand.

--- src/main/java/edu/tamu/srl/sketch/core/tobenamedlater/SrlPen.java
+++ src/main/java/edu/tamu/srl/sketch/core/tobenamedlater/SrlPen.java
@@ -1,5 +1,7 @@
 package edu.tamu.srl.sketch.core.tobenamedlater;
 
+import java.util.UUID;
+
 /**
  * Created by gigemjt on 11/3/14.
  * <br>
@@ -11,4 +13,52 @@ package edu.tamu.srl.sketch.core.tobenamedlater;
  * @author bpaulson
  */
 public class SrlPen {
+    /**
+     * Unique ID number for this pen
+     */
+    private UUID mId = UUID.randomUUID();
+
+    /**
+     * Manufacturer's pen ID number
+     */
+    private String mPenID = null;
+
+    /**
+     * Brand of the pen
+     */
+    private String mBrand = null;
+
+    /**
+     * Description of the pen
+     */
+    private String mDescription = null;
+
+    /**
+     * true if the pen is a finger
+     */
+    private boolean mPenIsFinger = false;
+
+    /**
+     * the digit of the pen that created the stroke.
+     * For instance if a device supports multiple pens this will differentiate them even if the author is the same.
+     * A more common example is if the user is using touch input and this will decide which finger it is.
+     * <pre>
+                __ __              __ __
+               /  V  \ _ _    _ _ /  V  \
+              |   |  |/ V \  / V \|  |   |
+              |   |  |  |  ||  |  |  |   |
+              |   |  |  |  ||  |  |  |   |
+              |   |  |  |  ||  |  |  |   |
+              |   |  |  |  ||  |  |  |   |
+              |   |  |  |  ||  |  |  |   |
+     _____   |          |  ||  |          |   _____
+    |     ''-/             ||             \-''     |
+     \_                    ||                    _/
+       ''-_     \          ||          /     _-''
+           \     )         /\         (     /
+            \             /  \             /
+            |            |    |            |
+     * </pre>
+     */
+    private boolean mPenDigit = -1;
 }
